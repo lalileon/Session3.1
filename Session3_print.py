2a416ffbe24952ddfda270d15c58d08a981ee3c1
lalileon <leonweiss3@gmail.com>
2023-02-02 01:55:52 +0100

    this is session 3 examples

---
+++ Session3_print.py
@@ -0,0 +1,10 @@
+print("This is a text")
+print("this is another")
+print("this","can","be","written","like","this")
+print("this",2+2, "be","written", "like","this", sep="!")
+#sep command determines the spaces inbetween; can be one or more characters
+print("this",2+2, "be","written", "like","this", sep="! ba 23")
+print("This is a text", end="|I want this at the end|") #end command replaces enter at the end
+print("This is a text")
+print("This is a text", end="|I want this at the end|\n") #end command replaces enter at the end adding \n adds enter
+print("This is a text")
